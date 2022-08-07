b60c166173e4635b4bd9421a943a61d5372ec666
David Krach <david.krach@mib.uni-stuttgart.de>
2022-08-08 01:42:15 +0200

    test works

--- sph-simulations/00_test/test.py
+++ sph-simulations/00_test/test.py
@@ -3,11 +3,43 @@ import hoomd
 from hoomd import *
 from hoomd import sph
 import numpy as np
-# import gsd.hoomd
+import gsd.hoomd
 
 cpu = hoomd.device.CPU()
 sim = hoomd.Simulation(device=cpu)
 
-integrator = hoomd.sph.Integrator(dt=0.005)
+
 
 # sim.operations.integrator = hoomd.sph.Integrator()
+
+
+#  ________________________________
+#               ---> FX             0.001 m
+#  ________________________________  
+#
+
+# System sizes
+LREF = 0.001                    # m
+
+LX = LREF*2
+LY = LREF*0.5
+LZ = LREF*0.5
+
+# Parameters
+KERNEL  = 'WendlandC4'
+NL      = 20                       # INT
+FX      = 0.1                      # m/s^2
+
+DX      = LREF/NL                  # m
+V       = DX*DX*DX                 # m^3
+
+RHO0 = 1000.0                      # kg / m^3
+M    = RHO0*V                      # kg
+DRHO = 0.05                        # %
+MU   = 0.01                        # Pa s
+
+UREF = FX*LREF*LREF*0.25/(MU/RHO0)
+
+H       = hoomd.sph.kernel.OptimalH[KERNEL]*DX       # m
+RCUT    = hoomd.sph.kernel.Kappa[KERNEL]*H           # m
+
