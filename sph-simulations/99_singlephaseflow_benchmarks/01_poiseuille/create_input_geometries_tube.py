95ede776dd2f04ac75f37ba521911e156a9e5fd4
David Krach <david.krach@mib.uni-stuttgart.de>
2023-10-06 16:45:08 +0200

    central location

--- sph-simulations/99_singlephaseflow_benchmarks/01_poiseuille/create_input_geometries_tube.py
+++ sph-simulations/99_singlephaseflow_benchmarks/01_poiseuille/create_input_geometries_tube.py
@@ -56,12 +56,11 @@ box_lx, box_ly, box_lz = lx, ly, lz
 n_particles = nx * ny * nz 
 
 # define meshgrid and add properties
-x, y, z = np.meshgrid(*(np.linspace(-box_lx / 2, box_lx / 2, nx, endpoint=True),),
-                      *(np.linspace(-box_ly / 2, box_ly / 2, ny, endpoint=True),),
-                      *(np.linspace(-box_lz / 2, box_lz / 2, nz, endpoint=True),))
+x, y, z = np.meshgrid(*(np.linspace(-box_lx / 2 + (dx/2), box_lx / 2 - (dx/2), nx, endpoint=True),),
+                      *(np.linspace(-box_ly / 2 + (dx/2), box_ly / 2 - (dx/2), ny, endpoint=True),),
+                      *(np.linspace(-box_lz / 2 + (dx/2), box_lz / 2 - (dx/2), nz, endpoint=True),))
 
 positions = np.array((x.ravel(), y.ravel(), z.ravel())).T
-
 
 velocities = np.zeros((positions.shape[0], positions.shape[1]), dtype = np.float32)
 masses     = np.ones((positions.shape[0]), dtype = np.float32) * mass
