e14af6ac1e29eb8f88f9902d47f347503e979ce8
David Krach <david.krach@mib.uni-stuttgart.de>
2023-10-29 11:56:51 +0100

    adapted path vars

--- addsphpath.sh
+++ addsphpath.sh
@@ -5,6 +5,7 @@ export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/hoomd-blue/build
 # export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/gsd-2.7.0/build
 # export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/gsd-2.8.0/build
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/gsd-3.2.0/build
+export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/dependencies/pgsd-3.2.0/build
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/helper_modules/gsd2vtu
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/helper_modules/delete_solid_sphparticles
 export PYTHONPATH=$PYTHONPATH:${GIT_SRC}/helper_modules/read_input_from_txt
