0e6f3f8cfe70a3ad11d99a1e092d3151090c652f
David Krach <david.krach@mib.uni-stuttgart.de>
2024-01-18 15:33:44 +0100

    helper tools added

--- link_pgsd_module.sh
+++ link_pgsd_module.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+git submodule foreach --recursive git checkout main 
+
 export GIT_SRC=$(pwd)
 ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd-3.2.0/pgsd/pgsd.c ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.c
 ln -s ${GIT_SRC}/dependencies/pgsd-sph/pgsd-3.2.0/pgsd/pgsd.h ${GIT_SRC}/hoomd-blue/hoomd/extern/pgsd.h
